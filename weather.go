39c4921b1e7f2aa15531ff11b674d8579bdc1c0c
Derek Chavez <frrenose17@yahoo.com>
2024-03-20 11:51:55 -0600

    feat: change main func to use http

--- weather.go
+++ weather.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"text/template"
 
 	"github.com/go-redis/redis/v8"
 )
@@ -21,10 +22,10 @@ type WeatherController struct {
 
 type Period struct {
 	Temperature float64 `json:"temperature"`
-	Properties struct{
+	Properties  struct {
 		RelativeHumidity float64 `json:"relativeHumidity"`
 	} `json:"properties"`
-	Pressure    float64 `json:"pressure"`
+	Pressure float64 `json:"pressure"`
 }
 
 func (wc *WeatherController) Update(temperature, humidity, pressure float64) {
@@ -42,27 +43,62 @@ func main() {
 	})
 	defer rdb.Close()
 
-	ctx := context.Background()
-
 	weatherData, err := fetchWeatherData()
 	if err != nil {
 		panic(err)
 	}
 
-	weatherController := WeatherController{}
-	weatherController.Update(weatherData.Temperature, weatherData.Humidity, weatherData.Pressure)
-
-	err = rdb.Set(ctx, "weather", fmt.Sprintf("temperature=%.2f humidity=%.2f pressure=%.2f", weatherController.WeatherData.Temperature, weatherController.WeatherData.Humidity, weatherController.WeatherData.Pressure), 0).Err()
-	if err != nil {
-		panic(err)
-	}
-	
-	val, err := rdb.Get(ctx, "weather").Result()
+	weatherJSON, err := json.Marshal(weatherData)
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println("Weather data retrieved from Redis cache:", val)
+	ctx := context.Background()
+	err = rdb.Set(ctx, "weather", weatherJSON, 0).Err()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Weather data has been cached")
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		val, err := rdb.Get(ctx, "weather").Result()
+		if err != nil {
+			http.Error(w, "Failed to retrieve weather data from Redis cache", http.StatusInternalServerError)
+			return
+		}
+
+		var weatherDataFromCache WeatherData
+		err = json.Unmarshal([]byte(val), &weatherDataFromCache)
+		if err != nil {
+			http.Error(w, "Failed to parse weather data from Redis cache", http.StatusInternalServerError)
+			return
+		}
+
+		tmpl := template.Must(template.New("weather").Parse(`
+			<!DOCTYPE html>
+			<html>
+			<head>
+				<title>Weather Data</title>
+			</head>
+			<body>
+				<h1>Weather Data</h1>
+				<p>Temperature: {{.Temperature}}</p>
+				<p>Humidity: {{.Humidity}}</p>
+				<p>Pressure: {{.Pressure}}</p>
+			</body>
+			</html>
+		`))
+
+		err = tmpl.Execute(w, weatherDataFromCache)
+		if err != nil {
+			http.Error(w, "Failed to render weather data template", http.StatusInternalServerError)
+			return
+		}
+	})
+
+	fmt.Println("Server is running on http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
 }
 
 func fetchWeatherData() (WeatherData, error) {
